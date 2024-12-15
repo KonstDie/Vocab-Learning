02fd72c49aca41d714b6aa6b515afcb7fd7f38cb
KonstDie <79140815+KonstDie@users.noreply.github.com>
2024-12-15 22:40:26 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let dictionary = {
-
 }
+let german = document.getElementById('german').innerHTML
+let english =  document.getElementById('enlish').innerHTML
 function addVoab{
     dictionary['german']= 'english'
 }
